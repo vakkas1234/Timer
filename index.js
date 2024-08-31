262b7ef78aa5efcfbd14c4ab324941adfc2ccb72
Vakkas Emre Yıldız <72315833+vakkas1234@users.noreply.github.com>
2024-08-31 15:23:56 +0300

    commit1

---
+++ index.js
@@ -0,0 +1,10 @@
+function click1(){
+    var p1 =   document.getElementById("1")
+    p1.style.visibility = "visible"
+    var i = 60
+    for(let i = 60; i>0 ;i-- ){
+     console.log
+    }
+        
+    }
+    
